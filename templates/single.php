eaa507e8099deec67ccc5ce35b6022317b4e5784
azX-45 <wazxwooz@gmail.com>
2020-08-11 12:41:06 +0200

    mise en  place du model

--- templates/single.php
+++ templates/single.php
@@ -1,9 +1,3 @@
-<?php
-require '../vendor/autoload.php';
-
-use App\src\DAO\ArticleDAO;
-use App\src\DAO\CommentDAO;
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -15,36 +9,25 @@ use App\src\DAO\CommentDAO;
 <div>
     <h1>Mon blog</h1>
     <p>En construction</p>
-    <?php
-    $article = new ArticleDAO();
-    $articles = $article->getArticle($_GET['articleId']);
-    $article = $articles->fetch()
-    ?>
     <div>
-        <h2><?= htmlspecialchars($article->title);?></h2>
-        <p><?= htmlspecialchars($article->content);?></p>
-        <p><?= htmlspecialchars($article->author);?></p>
-        <p>Créé le : <?= htmlspecialchars($article->createdAt);?></p>
+        <h2><?= htmlspecialchars($article->getTitle());?></h2>
+        <p><?= htmlspecialchars($article->getContent());?></p>
+        <p><?= htmlspecialchars($article->getAuthor());?></p>
+        <p>Créé le : <?= htmlspecialchars($article->getCreatedAt());?></p>
     </div>
     <br>
-    <?php
-    $articles->closeCursor();
-    ?>
     <a href="../public/index.php">Retour à l'accueil</a>
     <div id="comments" class="text-left" style="margin-left: 50px">
         <h3>Commentaires</h3>
         <?php
-        $comment = new CommentDAO();
-        $comments = $comment->getCommentsFromArticle($_GET['articleId']);
-        while($comment = $comments->fetch())
+        foreach ($comments as $comment)
         {
             ?>
-            <h4><?= htmlspecialchars($comment->pseudo);?></h4>
-            <p><?= htmlspecialchars($comment->content);?></p>
-            <p>Posté le <?= htmlspecialchars($comment->createdAt);?></p>
+            <h4><?= htmlspecialchars($comment->getPseudo());?></h4>
+            <p><?= htmlspecialchars($comment->getContent());?></p>
+            <p>Posté le <?= htmlspecialchars($comment->getCreatedAt());?></p>
             <?php
         }
-        $comments->closeCursor();
         ?>
     </div>
 </div>
