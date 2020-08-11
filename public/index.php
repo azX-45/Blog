80cba574d08c1ad9ad679ba72084ee44dbd0bf02
azX-45 <wazxwooz@gmail.com>
2020-08-11 09:50:33 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog de Jean Forteroche</title>
+</head>
+<body>
+  <h1>Bonjour tout le monde</h1>
+  <p>salut</p>
+  <h2>Code de la branche dev </h2>
+</body>
+</html>
