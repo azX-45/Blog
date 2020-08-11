613b608030dfeefe9d96c47482d76e783f97bd08
azX-45 <wazxwooz@gmail.com>
2020-08-11 10:43:30 +0200

    ajout de Comment.php

--- single.php
+++ single.php
@@ -3,6 +3,8 @@
 require 'Database.php';
 //Ne pas oublier d'ajouter le fichier Article.php
 require 'Article.php';
+//Ne pas oublier d'ajouter le fichier Comment.php
+require 'Comment.php';
 ?>
 
 <!DOCTYPE html>
@@ -22,16 +24,32 @@ require 'Article.php';
     $article = $articles->fetch()
     ?>
     <div>
-        <h2><?= htmlspecialchars($article['title']);?></h2>
-        <p><?= htmlspecialchars($article['content']);?></p>
-        <p><?= htmlspecialchars($article['author']);?></p>
-        <p>Créé le : <?= htmlspecialchars($article['createdAt']);?></p>
+        <h2><?= htmlspecialchars($article->title);?></h2>
+        <p><?= htmlspecialchars($article->content);?></p>
+        <p><?= htmlspecialchars($article->author);?></p>
+        <p>Créé le : <?= htmlspecialchars($article->createdAt);?></p>
     </div>
     <br>
     <?php
     $articles->closeCursor();
     ?>
     <a href="home.php">Retour à l'accueil</a>
+    <div id="comments" class="text-left" style="margin-left: 50px">
+        <h3>Commentaires</h3>
+        <?php
+        $comment = new Comment();
+        $comments = $comment->getCommentsFromArticle($_GET['articleId']);
+        while($comment = $comments->fetch())
+        {
+            ?>
+            <h4><?= htmlspecialchars($comment->pseudo);?></h4>
+            <p><?= htmlspecialchars($comment->content);?></p>
+            <p>Posté le <?= htmlspecialchars($comment->createdAt);?></p>
+            <?php
+        }
+        $comments->closeCursor();
+        ?>
+    </div>
 </div>
 </body>
 </html>
