6fb22860a188e020aef88f1b8597ee4b5bf37df0
azX-45 <wazxwooz@gmail.com>
2020-08-11 12:53:44 +0200

    class controller

--- src/controller/Backcontroller.php
+++ src/controller/Backcontroller.php
@@ -2,7 +2,16 @@
 
 namespace App\src\controller;
 
-class BackController
+class BackController extends Controller
 {
-
+    public function addArticle($post)
+    {
+        if(isset($post['submit'])) {
+            $this->articleDAO->addArticle($post);
+            header('Location: ../public/index.php');
+        }
+        return $this->view->render('add_article', [
+            'post' => $post
+        ]);
+    }
 }
