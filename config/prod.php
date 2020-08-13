147bf74a5f0fe3d2f69446705d315520693da93b
azX-45 <wazxwooz@gmail.com>
2020-08-13 08:24:48 +0200

    Ajout du prod.php

---
+++ config/prod.php
@@ -0,0 +1,8 @@
+<?php
+/** TODO : connexion db modification dev prod */
+const HOST = 'localhost';
+const DB_NAME = 'blog';
+const CHARSET = 'utf8';
+const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
+const DB_USER = 'root';
+const DB_PASS = ''; 
