9b38fb34872c10657512a305a5080f5c43118371
azX-45 <wazxwooz@gmail.com>
2020-10-05 16:20:14 +0200

    coment fonction

--- src/model/User.php
+++ src/model/User.php
@@ -4,68 +4,120 @@ namespace App\src\model;
 
 class User
 {
-    
+    /**
+     * @var int
+     */
+
     private $id;
 
+    /**
+     * @var string
+     */
+
     private $pseudo;
+
+    /**
+     * @var string
+     */
     
     private $password;
 
+    /**
+     * @var \DateTime
+     */
+
     private $createdAt;
 
+    /**
+     * @var string
+     */
+
     private $role;
+
+    /**
+     * @return int
+     */
 
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * @param int $id
+     */
     
     public function setId($id)
     {
         $this->id = $id;
     }
 
-    
+    /**
+     * @return string
+     */
+
     public function getPseudo()
     {
         return $this->pseudo;
     }
 
+    /**
+     * @return string $pseudo
+     */
     
     public function setPseudo($pseudo)
     {
         $this->pseudo = $pseudo;
     }
 
-    
+    /**
+     * @return string
+     */
+
     public function getPassword()
     {
         return $this->password;
     }
 
+    /**
+     * @return string $password
+     */
     
     public function setPassword($password)
     {
         $this->password = $password;
     }
 
-    
+    /**
+     * @return \DateTime
+     */
+
     public function getCreatedAt()
     {
         return $this->createdAt;
     }
 
+    /**
+     * @param \DateTime $createdAt
+     */
     
     public function setCreatedAt($createdAt)
     {
         $this->createdAt = $createdAt;
     }
 
+    /**
+     * @return string
+     */
+
     public function getRole()
     {
         return $this->role;
     }
+
+    /**
+     * @return string $role
+     */
 
     public function setRole($role)
     {
