fa39157e26a5364b42ae49e9ca2e8c6efb258205
Julien Serodio <67958474+azX-45@users.noreply.github.com>
2020-10-08 10:39:25 +0200

    Update prod.php

--- config/prod.php
+++ config/prod.php
@@ -5,4 +5,3 @@ const DB_NAME = 'serodiojrm554';
 const CHARSET = 'utf8';
 const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
 const DB_USER = 'serodiojrm554';
-const DB_PASS = 'Ovhslr45'; 
